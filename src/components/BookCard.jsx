176d11e1ec6d8b45b4edbaea512b84d778ceba4c
Eslam Al-Okda <al3kda1999@gmail.com>
2026-03-07 05:40:10 +0200

    Add Favorites Page

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { MdStar } from "react-icons/md";
 
-export default function BookCard({ bookName, author, reviwes, bookImg }) {
+export default function BookCard({ bookName, author, reviwes, bookImg,alt }) {
     return (
         <div className="flex flex-col gap-1 p-2 aspect-2/3 group">
             <div className="rounded-lg w-full h-full overflow-hidden flex-1 group-hover:-translate-y-2 transition duration-300">
-                <img src={bookImg} alt="circe" className="w-full h-full object-cover" />
+                <img src={bookImg} alt={alt} className="w-full h-full object-cover" />
             </div>
             <h2 className="text-lg capitalize line-clamp-1">{bookName}</h2>
             <p className="text-gray-600 dark:text-gray-400 capitaliz line-clamp-1e">{author}</p>
